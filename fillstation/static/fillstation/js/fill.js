7f5e6fb6883689cfc86fa90153525e9f1127f213
stpyang <stpyang@foursquare.com>
2016-06-16 10:27:28 -0400

    Disallow fills with psi_start < 0 or psi_end > 4000

--- fillstation/static/fillstation/js/fill.js
+++ fillstation/static/fillstation/js/fill.js
@@ -52,7 +52,7 @@ function addFill() {
   psi_end = Number(psi_end)
 
   // TODO(stpyang): how do I move this validation to python or html5?
-  if (psi_start >= psi_end) {
+  if (psi_start < 0 || psi_end > 4000 || psi_start >= psi_end) {
     return false
   }
 
