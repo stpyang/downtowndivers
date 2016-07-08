22d700d73f338236f84704157a5baf37ab1c94f3
stpyang <stpyang@foursquare.com>
2016-07-08 13:05:16 -0400

    Support for blending with 10/70

--- fillstation/forms.py
+++ fillstation/forms.py
@@ -111,7 +111,9 @@ class BlendForm(BlenderMixin, BillToMixin, TanksMixin, forms.Form):
     helium = forms.BooleanField()
     nitrox_32 = forms.BooleanField()
     nitrox_50 = forms.BooleanField()
+    trimix_10_70 = forms.BooleanField()
     trimix_18_45 = forms.BooleanField()
+    trimix_21_35 = forms.BooleanField()
 
     helium_start = forms.IntegerField(
         min_value=0,
