4f719d961ee8d510fbc80c09a5feabfb848791bc
Stephanie Yang <stpyang@post.harvard.edu>
2020-05-04 14:38:27 -0400

    Update documentation and quotes

--- ddny/settings/base.py
+++ ddny/settings/base.py
@@ -11,96 +11,96 @@ def get_env_variable(var_name):
     try:
         return os.environ[var_name]
     except KeyError: # pragma: no cover
-        error_msg = "Set the {0} environment variable".format(var_name)
+        error_msg = 'Set the {0} environment variable'.format(var_name)
         raise ImproperlyConfigured(error_msg)
 
 BASE_DIR = Path(__file__).ancestor(3)
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = get_env_variable("SECRET_KEY")
+SECRET_KEY = get_env_variable('SECRET_KEY')
 
 # Application definition
 
 INSTALLED_APPS = (
-    "grappelli",      # this must come before django.contrib.admin
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    # "debug_toolbar", # make stephanie's life easier
-    "extra_views",    # Inline forms
-    "widget_tweaks",  # add css to form tags
-    "ddny",
-    "ddny_braintree", # stuff for paying dues and fills
-    "ddny_calendar",  # shared events and iCal feed
-    # "debug",          # make stephanie's life easier
-    "fillstation",
-    "gas",            # gas info duh
-    "jsignature",     # for the consent form
-    "registration",   # member info
-    "tank",           # tank database
+    'grappelli',      # this must come before django.contrib.admin
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # 'debug_toolbar', # make stephanie's life easier
+    'extra_views',    # Inline forms
+    'widget_tweaks',  # add css to form tags
+    'ddny',
+    'ddny_braintree', # stuff for paying dues and fills
+    'ddny_calendar',  # shared events and iCal feed
+    # 'debug',          # make stephanie's life easier
+    'fillstation',
+    'gas',            # gas info duh
+    'jsignature',     # for the consent form
+    'registration',   # member info
+    'tank',           # tank database
 )
 
 MIDDLEWARE = (
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django.middleware.security.SecurityMiddleware",
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
 )
 
-ROOT_URLCONF = "ddny.urls"
+ROOT_URLCONF = 'ddny.urls'
 
 TEMPLATES = [{
-    "BACKEND": "django.template.backends.django.DjangoTemplates",
-    "DIRS": [
-        BASE_DIR.child("ddny").child("template"),
-        BASE_DIR.child("ddny_braintree").child("template"),
-        BASE_DIR.child("ddny_calendar").child("template"),
-        BASE_DIR.child("debug").child("template"),
-        BASE_DIR.child("fillstation").child("template"),
-        BASE_DIR.child("gas").child("template"),
-        BASE_DIR.child("jsignature").child("template"),
-        BASE_DIR.child("registration").child("template"),
-        BASE_DIR.child("tank").child("template"),
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [
+        BASE_DIR.child('ddny').child('template'),
+        BASE_DIR.child('ddny_braintree').child('template'),
+        BASE_DIR.child('ddny_calendar').child('template'),
+        BASE_DIR.child('debug').child('template'),
+        BASE_DIR.child('fillstation').child('template'),
+        BASE_DIR.child('gas').child('template'),
+        BASE_DIR.child('jsignature').child('template'),
+        BASE_DIR.child('registration').child('template'),
+        BASE_DIR.child('tank').child('template'),
     ],
-    "APP_DIRS": True,
-    "OPTIONS": {
-        "context_processors": [
-            "django.template.context_processors.debug",
-            "django.template.context_processors.request",
-            "django.contrib.auth.context_processors.auth",
-            "django.contrib.messages.context_processors.messages",
+    'APP_DIRS': True,
+    'OPTIONS': {
+        'context_processors': [
+            'django.template.context_processors.debug',
+            'django.template.context_processors.request',
+            'django.contrib.auth.context_processors.auth',
+            'django.contrib.messages.context_processors.messages',
         ],
     },
 }]
 
-WSGI_APPLICATION = "ddny.wsgi.application"
+WSGI_APPLICATION = 'ddny.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(),
+    'default': dj_database_url.config(),
 }
-DATABASES["default"]["ATOMIC_REQUESTS"] = True
+DATABASES['default']['ATOMIC_REQUESTS'] = True
 
 # I18n
-# https://docs.djangoproject.com/en/1.8/topics/i18n/
+# https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = "US/Eastern"
+TIME_ZONE = 'US/Eastern'
 
 USE_I18N = True
 
@@ -110,29 +110,29 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-# Honor the "X-Forwarded-Proto" header for request.is_secure()
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
 
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 
-ALLOWED_HOSTS = [".herokuapp.com", ".downtowndivers.org"]
+ALLOWED_HOSTS = ['.herokuapp.com', '.downtowndivers.org']
 
 # Static asset configuration
 
-STATIC_ROOT = BASE_DIR.child("staticfiles")
+STATIC_ROOT = BASE_DIR.child('staticfiles')
 
-STATIC_URL = "/static/"
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    BASE_DIR.child("ddny"),
+    BASE_DIR.child('ddny'),
 )
 
 # Settings for django.contrib.auth.
 
-LOGIN_URL = "/login/"
+LOGIN_URL = '/login/'
 
-LOGIN_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = '/'
 
--- staticfiles/settings/sandbox.py
+++ staticfiles/settings/sandbox.py
@@ -7,35 +7,35 @@ from .constants import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["ddny-sandbox.herokuapp.com", "*"]
+ALLOWED_HOSTS = ['ddny-sandbox.herokuapp.com']
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # DDNY settings
 
-DEFAULT_FROM_EMAIL = "gizmo.santore@gmail.com"
+DEFAULT_FROM_EMAIL = 'gizmo.santore@gmail.com'
 
-EMAIL_HOST = "smtp.sendgrid.net"
+EMAIL_HOST = 'smtp.sendgrid.net'
 
-EMAIL_HOST_USER = "ddny"
+EMAIL_HOST_USER = 'ddny'
 
-EMAIL_HOST_PASSWORD = get_env_variable("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_PASSWORD = get_env_variable('EMAIL_HOST_PASSWORD')
 
 EMAIL_PORT = 587
 
 EMAIL_USE_TLS = True
 
-TANK_NAZI = "stpyang@post.harvard.edu"
+TANK_NAZI = 'stpyang@post.harvard.edu'
 
-OOPS_EMAIL = "stpyang@post.harvard.edu"
+OOPS_EMAIL = 'stpyang@post.harvard.edu'
 
 # Braintree sandbox
 
 braintree.Configuration.configure(
     braintree.Environment.Sandbox,
-    merchant_id=get_env_variable("BRAINTREE_MERCHANT_ID"),
-    public_key=get_env_variable("BRAINTREE_PUBLIC_KEY"),
-    private_key=get_env_variable("BRAINTREE_PRIVATE_KEY"),
+    merchant_id=get_env_variable('BRAINTREE_MERCHANT_ID'),
+    public_key=get_env_variable('BRAINTREE_PUBLIC_KEY'),
+    private_key=get_env_variable('BRAINTREE_PRIVATE_KEY'),
 )
 
 BRAINTREE_CLIENT_TOKEN = braintree.ClientToken.generate()
