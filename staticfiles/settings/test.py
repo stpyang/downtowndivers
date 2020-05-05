a7c09e08b0e300a8445a8210293d32775ca47643
Stephanie Yang <stpyang@post.harvard.edu>
2020-05-05 16:46:09 -0400

    Add staticfiles

---
+++ staticfiles/settings/test.py
@@ -0,0 +1,11 @@
+'''Copyright 2020 Downtown Divers New York. All Rights Reserved.'''
+
+from .sandbox import *
+
+DATABASES = {
+	'default': {
+	    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+	    'NAME': 'postgres',
+	    'USER': 'django',
+	}
+}
