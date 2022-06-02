12fe6232b9ebad09904f4abdbbd3817ce3b8f8ec
Stephanie Yang <stpyang@post.harvard.edu>
2022-06-02 08:57:54 -0400

    Update price of oxygen to 45 cents

--- ddny/settings/costs.py
+++ ddny/settings/costs.py
@@ -13,6 +13,6 @@ EQUIPMENT_COST_PROPORTIONAL = Decimal(0.08).quantize(PENNY)
 AIR_COST = Decimal(0.03).quantize(PENNY)
 ARGON_COST = Decimal(1.25).quantize(PENNY)
 HELIUM_COST = Decimal(0.00).quantize(PENNY)
-OXYGEN_COST = Decimal(0.35).quantize(PENNY)
+OXYGEN_COST = Decimal(0.45).quantize(PENNY)
 
 MONTHLY_DUES = Decimal(37.00).quantize(PENNY)
